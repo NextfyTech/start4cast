c87d6f0326a4978b68b619b23e76c97a5124f78e
priya13997 <122154227+priya13997@users.noreply.github.com>
2023-03-23 23:09:49 -0500

    UI

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 
+<div class="container-fluid">
 <form class="form-horizontal" action="{{route('update',$data->spl_category_id)}}" method="POST" enctype="multipart/form-data">
     <h3>Edit Categories</h3>
     @csrf
- <fieldset>
-  <legend>Add Data - Special Categories</legend>
-  
+
 
   </div>
   <div class="form-group">
   <div class=" row">
-    <label for="specialCategory" class="col-sm-3">Special Category:</label>
-    <div class="col">
+    <label class="control-label col-sm-2" for="specialCategory" class="col-sm-3">Special Category:</label>
+    <div class="col-sm-10">
       <input type="text" class="form-control" name="spl_category" id="spl_category" value="{{$data->spl_category}}">
     </div>
   </div>
 <button class="btn btn-primary" type="submit">Update Data</button>
 
 </form>
+</div>
 @endsection
